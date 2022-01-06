b1dea2a914f349c78e8d910f91b51ef7238047aa
alper.turgal <alper.turgal@gmail.com>
2022-01-06 20:35:42 +0300

    running properly before search functionality

--- time_sharing_v3/settings.py
+++ time_sharing_v3/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'time_sharing_v3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_v1',
-        'USER': 'test',
-        'HOST': 'db',
-        'PASSWORD': "password",
         'PORT': '5432',
     }
 }
